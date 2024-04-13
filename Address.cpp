ee07814ecdc866d366e51b1898ac21e9baeb92ac
Aimen Malik <Aimo.Malik77@gmail.com>
2024-04-13 23:22:30 +0500

    Update Address.cpp

--- Address.cpp
+++ Address.cpp
@@ -1,43 +1,63 @@
 #include "Address.h"
 
+// Constructors
+Address::Address() {
+	house = "";
+	street = "";
+	city  = "";
+	country = "";
+}
 
-/*
-*	Implement a constructor that initializes all the address parts
-*
-*/
+Address::Address(std::string house, std::string street, std::string city, std::string country) {
+	this->house = house;
+	this->street = street;
+	this->city = city;
+	this->country = country;
+}
+
+// Getters
+std::string Address::get_house() const { return house; }
+std::string Address::get_street() const { return street; }
+std::string Address::get_city() const { return city; }
+std::string Address::get_country() const { return country; }
+
+// Setters
+void Address::set_house(std::string house){
+	this->house = house;
+}
+void Address::set_street(std::string street){
+	this->street = street;
+}
+void Address::set_city(std::string city){
+	this->city = city;
+}
+void Address::set_country(std::string country){
+	this->country = country;
+}
 
 
-/*
-*	Implement getter/setters for all the member variables of Address. Also declare them in the Address.h header file
-*
-*/
-
-
-
-/*
-*	Implement the equals function that takes an Address object and checks if it is equal to
-*	current object refered by (this pointer)
-*	Two addresses are equal if all the attributes of addresses are equal
-*/
 bool Address::equals(const Address& address)
 {
-	/*
-	* Todo
-	*/
-	return false;
+	if (this == &address) {
+		return true;
+	}
+	if (this->house != address.house ||
+		this->street != address.street ||
+		this->city != address.city ||
+		this->country != address.country)
+	{
+		return false;
+	}
+	return true;
 }
 
 void Address::print_address()
 {
-	/*
-	*	Print address on one line as
-	*	House, Street, City, Country
-	*/
+	std::cout << house << ", " << street << ", " << city << ", " << country << std::endl;
 }
 
 Address Address::copy_address()
 {
-	/*
-		return a copy of this
-	*/
+	// Return a copy of current object
+	return Address(house, street, city, country);
 }
