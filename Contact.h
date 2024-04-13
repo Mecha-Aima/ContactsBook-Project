e33c354e36fc8065ce0f2e8fa5f283206b1de8d5
Aimen Malik <Aimo.Malik77@gmail.com>
2024-04-13 23:23:43 +0500

    Update Contact.h

--- Contact.h
+++ Contact.h
@@ -8,7 +8,6 @@
 
 #include "Address.h"
 
-
 class Contact {
 private:
 	std::string first_name;
@@ -23,4 +22,16 @@ public:
 
 	Contact() {}
 	Contact(std::string first_name, std::string last_name, std::string mobile_number, std::string email_address, Address *address);
+	// Setters
+	void set_first_name(std::string first_name);
+	void set_last_name(std::string last_name);
+	void set_mobile_number(std::string mobile_number);
+	void set_email_address(std::string email_address);
+	void set_address(Address *address);
+	// Getters
+	std::string get_first_name() const;
+	std::string get_last_name() const;
+	std::string get_mobile_number() const;
+	std::string get_email_address() const;
+	Address* get_address() const;
 };
