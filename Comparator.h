38be0bd64d6b49a28f06c005ae53dba2504b88bd
mecha-aima <aimo.malik77@gmail.com>
2024-05-02 10:05:17 +0500

    comparator

---
+++ Comparator.h
@@ -0,0 +1,15 @@
+#pragma once
+
+template <typename T>
+class Comparator
+{
+public:
+    virtual bool compare(const T& b, bool (*fptr)(const T& a, const T& b)) const = 0;
+};
+
+// Use cases:
+// 1. Compare contact's names (first name, last name)
+// 2. Compare contact's email addresses
+// 3. Compare two time objects
+// 4. Compare two date objects
+// 5. Compare two History objects
