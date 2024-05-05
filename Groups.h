6b64eedcc44f9428db09fa14127beca8734461e6
Mudasar Bhatti <134007191+bhatticoder@users.noreply.github.com>
2024-05-05 13:32:39 +0500

    Groups.h

---
+++ Groups.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef BASIC_LIB
+#define BASIC_LIB
+#include "contactsBook.h"
+#include <iostream>
+#include <string>
+#define max_Contacts 100
+using namespace std;
+class Groups {
+private:
+	string name;
+	int contactIDs[max_Contacts];
+	int numberOfContacts;
+public:
+	Groups();
+	void add_contact(int contactId);
+	void remove_contact(int contactID);
+    // Method to get the name of the group
+	string getName();
+    // Method to get the number of contacts in the group
+	int getNumberOfContacts();
+    // Method to get the list of contact IDs in the group
+	int* getContactIDs();
+};
+#endif
