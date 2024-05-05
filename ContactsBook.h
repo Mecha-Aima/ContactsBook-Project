c2a011e65e5717e209ffbd12950ae48dd3ab32bb
Mudasar Bhatti <134007191+bhatticoder@users.noreply.github.com>
2024-05-05 13:30:03 +0500

    ContactsBook.h

--- ContactsBook.h
+++ ContactsBook.h
@@ -1,43 +1,36 @@
-#pragma once
-#include "Address.h"
-#include "Contact.h"
-
 #ifndef BASIC_LIB
 #define BASIC_LIB
+#endif // !BASIC_LIB
+#include "Contact.h"
+#include <iomanip>
 #include <iostream>
 #include <string>
-#endif // !BASIC_LIB
-
-
 class ContactsBook {
 private:
-	Contact *contacts_list; // array of contacts
+	Contact* contacts_list; // array of contacts
 	int size_of_contacts;   // storage limit
 	int contacts_count;     // total contacts currently stored, next contact will be 
-						    // stored at this count plus 1 index
-
+	// stored at this count plus 1 index
 public:
 	void add_contact(const Contact& contact);
-	int total_contacts();
-
+	int total_contacts() const;
 	Contact* search_contact(std::string first_name, std::string last_name);
 	Contact* search_contact(std::string phone);
 	Contact* search_contact(const Address& address);
-
 	ContactsBook(int size_of_list);
-
+	~ContactsBook();
+	ContactsBook(const ContactsBook& other);
 	void print_contacts_sorted(std::string choice); // Only two choices first_name or last_name
-	void merge_duplicates(); // Implement this function that finds and merges all the duplicates
-							 // Duplicate means exact equal, this means if
-	
-	/* 
-	*  This function loads contacts from the given file (see details in ContactsBook.cpp)
-	*/
+	void merge_duplicates(); // Merge duplicates
+	void print_contacts() const;	// Print all contacts in the order they were added
 	void load_from_file(std::string file_name);
 	void save_to_file(std::string file_name);
-
+	Contact* copy_list(Contact* contacts_list);
+	// Overload assignment operator
+	ContactsBook& operator=(const ContactsBook& other);
 private:
-	bool full();
+	bool full() const;
 	void resize_list();
-	void sort_contacts_list(Contact *contacts_list, std::string choice);
+	void sort_contacts_list(Contact* contacts_list, std::string choice);
+
 };
