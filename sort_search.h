f21482af80348357d2e767620e229a3f00cf4b1b
mecha-aima <aimo.malik77@gmail.com>
2024-05-07 00:07:38 +0500

    new list sort template

--- sort_search.h
+++ sort_search.h
@@ -1,4 +1,5 @@
 #include "Comparator.h"
+#include "list.h"
 
 template <typename C>
 void sort_list(C* arr, int n, Comparator<C>* comp, bool (*fptr)(const C& a, const C& b))
@@ -16,3 +17,22 @@ void sort_list(C* arr, int n, Comparator<C>* comp, bool (*fptr)(const C& a, cons
         }
     }
 }
+
+template <typename C>
+void sort_list(List<C>& arr, Comparator<C>* comp, bool (*fptr)(const C& a, const C& b))
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+           if (comp->compare(arr[j+1], arr[j], fptr))
+           {
+               C temp = arr[j];
+               arr[j] = arr[j+1];
+               arr[j+1] = temp;
+           }
+        }
+    }
+}
+
