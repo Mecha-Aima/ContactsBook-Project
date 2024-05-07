ddcc5e62ad1a9f46d7583929804f8d917fbed0bb
Aimen Malik <Aimo.Malik77@gmail.com>
2024-05-07 05:15:45 +0500

    Update AdvancedSearch.h

--- AdvancedSearch.h
+++ AdvancedSearch.h
@@ -1,22 +1,10 @@
-#pragma once
-#pragma once
-#ifndef ADVANCESEARCH_H
-#define ADVANCESEARCH_H
+#ifndef ADVANCEDSEARCH_H
+#define ADVANCEDSEARCH_H
 #include "HistoryObject.h"
-<<<<<<< HEAD
 #include "SearchHistory.h"
 #include "ContactsBook.h"
-#include "Contact.h"
 #include "List.h"
 #include <string>
-
-=======
-#include "ContactsBook.h"
-#include "Contact.h"
-#include <string.h>
-#include <cstring>
-#include "List.h"
->>>>>>> 1063c0e4201d08ac3ca1c98f17b17fdb2938afe2
 
 class AdvanceSearch {
 private:
@@ -30,12 +18,10 @@ public:
     ~AdvanceSearch();
     // Method to perform the search
     void perform_search(std::string query,ContactsBook& book);
-<<<<<<< HEAD
     List<string> get_results();
     void update_history(HistoryObject &h, SearchHistory &sh);
 };
-#endif // ADVANCESEARCH_H
-=======
+#endif // ADVANCEDSEARCH_H
     
     void display_results();
 };
